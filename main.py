8564fda702b658865c8f040c24a3bc933dc6d489
Phill Conrad <phtcon@ucsb.edu>
2021-01-28 00:48:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+
+import spacy
+import requests
+from bs4 import BeautifulSoup
+# From https://docs.repl.it/tutorials/03-handling-dependencies-with-repl-it
+
+from collections import Counter
+
+nlp = spacy.load("en_core_web_sm")
+response = requests.get("http://lite.cnn.com/en")
+soup = BeautifulSoup(response.text, "html.parser")
+
+# https://stackoverflow.com/questions/1936466/beautifulsoup-grab-visible-webpage-text
+[s.extract() for s in soup(['style', 'script', '[document]', 'head', 'title'])]
+text = soup.getText()
+doc = nlp(text)
+
+names = []
+for ent in doc.ents:
+    if ent.label_ == "PERSON":
+        names.append(ent.lemma_)
+
+print("These people are in the headlines today")
+print(Counter(names).most_common(10))
